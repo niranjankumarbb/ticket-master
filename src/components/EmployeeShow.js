3f0df8e6c2f072f893dd40efe4470f57cb4879f7
niranjankumarbb <niranjan32@gmail.com>
2021-04-24 18:55:10 +0530

    updated

--- src/components/EmployeeShow.js
+++ src/components/EmployeeShow.js
@@ -116,8 +116,8 @@ class EmployeeShow extends React.Component{
                                           })
                                         }
                                      </React.Fragment>
-                                 )
-                             }    
+                                   )
+                               }    
                   </tbody>                        
                </table>
              </div>
